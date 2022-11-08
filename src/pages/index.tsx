feadc943a16116d5fc32f29ee90f0382241041a2
Tiago Costa <tiagodpv@gmail.com>
2022-11-07 21:26:28 -0500

    add header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,26 @@
 import { styled } from '../styles'
 
 const Button = styled('button', {
-  backgroundColor: '$rocketseat',
+  backgroundColor: '$green300',
   borderRadius: 4,
   border: 0,
-  padding: '4px 8px'
+  padding: '4px 8px',
+
+  span: {
+    fontWeight: 'bold',
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)'
+  }
 })
 
 
 export default function Home() {
   return (
-    <Button>Hello World</Button>
+    <Button>
+      <span>Teste </span>
+      Enviar
+    </Button>
   )
 }
