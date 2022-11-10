983d7caabc0b8098b6b9a22dd146286825dde423
Tiago Costa <tiagodpv@gmail.com>
2022-11-09 22:00:03 -0500

    add  product data

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,10 +1,34 @@
+import { GetStaticProps } from "next"
 import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 
 
 export default function Product() {
   const { query } = useRouter ()
 
   return(
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>        
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime ab eaque cum dolorem ipsum beatae iure debitis aliquid laborum pariatur repellendus neque reiciendis, velit, iusto nisi illum expedita? Nesciunt, dolorum.</p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
   )
+}
+
+export const getStaticProps: GetStaticProps =  async ({params}) => {
+  //const productId = params.id;
+  
+  return {
+    props: {},
+    revalidate: 60* 60 * 1 // 1 hour
+  }
 }
